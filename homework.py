7a316e9baea4a0e31d89b63bd57657e5617e5989
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-10-31 16:29:09 +0500

    added another code

--- homework.py
+++ homework.py
@@ -417,3 +417,26 @@
 # ismlar = ['ali', 'vali', 'hasan', 'husan']
 # katta_harf(ismlar)
 # print(ismlar)
+
+import random as r
+
+print("Omad show o'yiniga xush kelibsiz!")
+
+mijozlar = []
+
+print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+n=1 # ismlarni sanash uchun o'zgaruvchi
+while True:
+    savol = f"{n}-mahsulot xarid qilgan odam raqamini kiriting: "
+    mijoz = input(savol)
+    mijozlar.append(mijoz)
+    javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
+    if javob =='ha':
+        n+=1
+        continue
+    else:
+        break
+
+print(mijozlar)
+
+print(f"Hurmatli {r.choice(mijozlar)} raqam egasi sizni keyingi ko'rsatuvga taklif qilamiz!")
