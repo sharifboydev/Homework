3d107b865677a395277586f6e04e9ba53db8b9f7
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-10-31 16:39:20 +0500

    changes code

--- homework.py
+++ homework.py
@@ -425,14 +425,14 @@ print("Omad show o'yiniga xush kelibsiz!")
 mijozlar = []
 
 print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
-n=1 # ismlarni sanash uchun o'zgaruvchi
+n = 1  # ismlarni sanash uchun o'zgaruvchi
 while True:
     savol = f"{n}-mahsulot xarid qilgan odam raqamini kiriting: "
     mijoz = input(savol)
     mijozlar.append(mijoz)
     javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
-    if javob =='ha':
-        n+=1
+    if javob == 'ha':
+        n += 1
         continue
     else:
         break
