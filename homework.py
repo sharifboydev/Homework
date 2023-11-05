27f78447f6999be259b42b7f3b78dc5be7578eaf
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-06 00:18:16 +0500

    changes code

--- homework.py
+++ homework.py
@@ -420,23 +420,25 @@
 
 import random as r
 
-print("Omad show o'yiniga xush kelibsiz!")
+# print("Omad show o'yiniga xush kelibsiz!")
+#
+# mijozlar = []
+#
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# n = 1  # ismlarni sanash uchun o'zgaruvchi
+# while True:
+#     savol = f"{n}-mahsulot xarid qilgan odam raqamini kiriting: "
+#     mijoz = input(savol)
+#     mijozlar.append(mijoz)
+#     javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
+#     if javob == 'ha':
+#         n += 1
+#         continue
+#     else:
+#         break
 
-mijozlar = []
+# print(mijozlar)
 
-print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
-n = 1  # ismlarni sanash uchun o'zgaruvchi
-while True:
-    savol = f"{n}-mahsulot xarid qilgan odam raqamini kiriting: "
-    mijoz = input(savol)
-    mijozlar.append(mijoz)
-    javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
-    if javob == 'ha':
-        n += 1
-        continue
-    else:
-        break
+# print(f"Hurmatli {r.choice(mijozlar)} raqam egasi sizni keyingi ko'rsatuvga taklif qilamiz!")
 
-print(mijozlar)
 
-print(f"Hurmatli {r.choice(mijozlar)} raqam egasi sizni keyingi ko'rsatuvga taklif qilamiz!")
