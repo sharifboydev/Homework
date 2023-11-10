d5caab7dde0334c20603f48d8e1eefcfa0dc5067
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-10 14:22:01 +0500

    added new code

--- homework.py
+++ homework.py
@@ -470,3 +470,38 @@ avto1 = Avto("Black", "BMW", "$100000", "avtomat", 2015)
 print(avto1.get_info())
 avto1.update_km()
 print(avto1.get_info())
+
+
+class Avto:
+
+    def __init__(self, make, model, rang, yil, narh, km=0):
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        self.__km = km
+        self.__id = uuid4()
+
+    def get_km(self):
+        return self.__km
+
+    def get_id(self):
+        return self.__id
+
+    def add_km(self, km):
+        if km >= 0:
+            self.__km += km
+        else:
+            print("mashina km qaytarib bo'lmaydi.")
+
+
+avto1 = Avto("Gm", "Malibu", "oq", "2015", 40000, 100000)
+avto1.add_km(15000)
+print(avto1.get_km())
+
+print(f"ID: {avto1.get_id()}")
+print(f"KM: {avto1.get_km()}")
+
+x, y = 2, 3
+print(x > y)
